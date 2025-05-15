481df6683f06f58ce52c8a7e7f375a53baf313ac
JauniusPinelis <JauniusPinelis@gmail.com>
2025-05-15 19:01:23 +0300

    update

--- python_concepts.py
+++ python_concepts.py
@@ -1,22 +1,25 @@
 from datetime import datetime
 
-
 # Variables and Data Types
 # Variables are used to store data values
 # This is a comment
-a = 10 # This is an integer
+
+a = 1 # This is an integer
 b = 'hello' # This is a string
 c = 3.14 # This is a float
-d = True
+d = True # boolean value
+e = "True"
 
 # print(b + ' Jaunius')  
 
 # main console functions
-console_input = input('Enter your name: ')
-print('Hello, ' + console_input)
+# console_input = input('Enter your name: ')
+# print('Hello, ' + console_input)
 
 # if statements
 if a > 5:
+    print('a is greater than 5')
+    print('a is greater than 5')
     print('a is greater than 5')
 elif a == 5:
     print('a is equal to 5')
@@ -26,14 +29,27 @@ else:
 # for loops
 for i in range(5):
     print('Iteration:', i)
-
+    print('Iteration:', i)
+    print('Iteration:', i)
     # Example: Calculate age from a date
+
+# while loops
+while i > 3:
+    print('While loop iteration:', i)
+    i += 1
+
+# try catch
+try:
+    # Attempt to convert a string to an integer
+    num = int('abc')  # This will raise a ValueError
+except Exception as e:
+    print("An unexpected error occurred:", str(e))
 
 # Get current date
 current_date = datetime.now()
 
 # Example birthdate (year, month, day)
-birth_date = datetime(1990, 5, 15)
+birth_date = datetime(1990, 1, 14)
 
 # Calculate age
 age = current_date - birth_date
